1ee45a913dc8e59a6e2189a15d01563ea76f199e
Sam <sami.t.arar@hotmail.com>
2022-05-22 00:02:26 -0400

    Fixed bug with yahoo api

--- client/src/actions/yahooActions.js
+++ client/src/actions/yahooActions.js
@@ -1,75 +1,70 @@
-import axios from "axios";
-import {
-  GET_TICKER_DATA,
-  TICKER_DATA_LOADING,
-  GET_SEARCH_DATA,
-  SEARCH_DATA_LOADING,
-} from "./types";
+import axios from 'axios';
+import { GET_TICKER_DATA, TICKER_DATA_LOADING, GET_SEARCH_DATA, SEARCH_DATA_LOADING } from './types';
 
 export const searchStock = (symbol) => {
-  return async (dispatch) => {
-    dispatch(setSearchDataLoading());
-    symbol = symbol.toUpperCase();
-    const response = await axios
-      .get("/api/yahoo-finance/search", {
-        params: {
-          symbol: symbol,
-        },
-      })
-      .then((res) =>
-        dispatch({
-          type: GET_SEARCH_DATA,
-          payload: res.data,
-        })
-      )
-      .catch((err) =>
-        dispatch({
-          type: GET_SEARCH_DATA,
-          payload: null,
-        })
-      );
+    return async (dispatch) => {
+        dispatch(setSearchDataLoading());
+        symbol = symbol.toUpperCase();
+        const response = await axios
+            .get('/api/yahoo-finance/search', {
+                params: {
+                    symbol: symbol
+                }
+            })
+            .then((res) =>
+                dispatch({
+                    type: GET_SEARCH_DATA,
+                    payload: res.data
+                })
+            )
+            .catch((err) =>
+                dispatch({
+                    type: GET_SEARCH_DATA,
+                    payload: null
+                })
+            );
 
-    return response;
-  };
+        return response;
+    };
 };
 
 // Get ticker symbol data
 export const getTickerData = (data) => {
-  return async (dispatch) => {
-    dispatch(setTickerDataLoading());
-    const response = await axios
-      .get("api/yahoo-finance", {
-        params: {
-          symbol: data.symbol,
-        },
-      })
-      .then((res) =>
-        dispatch({
-          type: GET_TICKER_DATA,
-          payload: res.data,
-        })
-      )
-      .catch((err) =>
-        dispatch({
-          type: GET_TICKER_DATA,
-          payload: null,
-        })
-      );
+    return async (dispatch) => {
+        dispatch(setTickerDataLoading());
+        const response = await axios
+            .get('api/yahoo-finance', {
+                params: {
+                    symbol: data.symbol
+                }
+            })
+            .then((res) =>
+                dispatch({
+                    type: GET_TICKER_DATA,
+                    payload: res.data
+                })
+            )
+            .catch((err) =>
+                dispatch({
+                    type: GET_TICKER_DATA,
+                    payload: null
+                })
+            );
 
-    return response;
-  };
+        return response;
+    };
 };
 
 // Ticker data loading
 export const setTickerDataLoading = () => {
-  return {
-    type: TICKER_DATA_LOADING,
-  };
+    return {
+        type: TICKER_DATA_LOADING
+    };
 };
 
 // Ticker data loading
 export const setSearchDataLoading = () => {
-  return {
-    type: SEARCH_DATA_LOADING,
-  };
+    return {
+        type: SEARCH_DATA_LOADING
+    };
 };
--- routes/api/yahoo-finance.js
+++ routes/api/yahoo-finance.js
@@ -1,89 +1,79 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const yahooFinance = require("yahoo-finance2").default;
+const passport = require('passport');
+const yahooFinance = require('yahoo-finance2').default;
 
 // @route GET api/yahoo-finance
 // @desc Get a ticker symbol data
 // @access Private
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     yahooFinance
-      .quote(req.query.symbol, {
-        fields: [
-          // "symbol",
-          // "displayName",
-          // "marketCap",
-          // "regularMarketPrice",
-          // "regularMarketChangePercent",
-          // "regularMarketChange",
-          // "regularMarketDayHigh",
-          // "regularMarketDayLow",
-          // "regularMarketOpen",
-          // "regularMarketPreviousClose",
-          // "regularMarketVolume",
-          // "averageDailyVolume3Month",
-          // "fiftyTwoWeekLow",
-          // "fiftyTwoWeekHigh",
-          // "fiftyTwoWeekLowChange",
-          // "fiftyTwoWeekHighChange",
-          // "sharesOutstanding",
-          // "fiftyTwoWeekLowChangePercent",
-          // "fiftyTwoWeekHighChangePercent",
-          // "bookValue",
-          // "dividendDate",
-          // "trailingAnnualDividendRate",
-          // "trailingAnnualDividendYield",
-          // "forwardPE",
-          // "priceToBook",
-          // "trailingPE",
-        ],
-      })
-      .then((tickerData) => {
-        yahooFinance
-          .quoteSummary(req.query.symbol, {
-            modules: ["summaryDetail", "defaultKeyStatistics"],
-          })
-          .then((tickerDataStats) => {
-            if (
-              tickerDataStats &&
-              tickerDataStats.defaultKeyStatistics &&
-              tickerDataStats.summaryDetail
-            ) {
-              tickerData["pegRatio"] =
-                tickerDataStats.defaultKeyStatistics.pegRatio;
+        .quote(
+            req.query.symbol,
+            {
+                fields: [
+                    // "symbol",
+                    // "displayName",
+                    // "marketCap",
+                    // "regularMarketPrice",
+                    // "regularMarketChangePercent",
+                    // "regularMarketChange",
+                    // "regularMarketDayHigh",
+                    // "regularMarketDayLow",
+                    // "regularMarketOpen",
+                    // "regularMarketPreviousClose",
+                    // "regularMarketVolume",
+                    // "averageDailyVolume3Month",
+                    // "fiftyTwoWeekLow",
+                    // "fiftyTwoWeekHigh",
+                    // "fiftyTwoWeekLowChange",
+                    // "fiftyTwoWeekHighChange",
+                    // "sharesOutstanding",
+                    // "fiftyTwoWeekLowChangePercent",
+                    // "fiftyTwoWeekHighChangePercent",
+                    // "bookValue",
+                    // "dividendDate",
+                    // "trailingAnnualDividendRate",
+                    // "trailingAnnualDividendYield",
+                    // "forwardPE",
+                    // "priceToBook",
+                    // "trailingPE",
+                ]
+            },
+            { validateResult: false }
+        )
+        .then((tickerData) => {
+            yahooFinance
+                .quoteSummary(req.query.symbol, {
+                    modules: ['summaryDetail', 'defaultKeyStatistics']
+                })
+                .then((tickerDataStats) => {
+                    if (tickerDataStats && tickerDataStats.defaultKeyStatistics && tickerDataStats.summaryDetail) {
+                        tickerData['pegRatio'] = tickerDataStats.defaultKeyStatistics.pegRatio;
 
-              tickerData["beta"] = tickerDataStats.summaryDetail.beta;
+                        tickerData['beta'] = tickerDataStats.summaryDetail.beta;
 
-              tickerData["priceToSalesTrailing12Months"] =
-                tickerDataStats.summaryDetail.priceToSalesTrailing12Months;
-            }
+                        tickerData['priceToSalesTrailing12Months'] = tickerDataStats.summaryDetail.priceToSalesTrailing12Months;
+                    }
 
-            res.json(tickerData);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.json(tickerData);
-          });
-      })
-      .catch((err) => console.log(err));
-  }
-);
+                    res.json(tickerData);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.json(tickerData);
+                });
+        })
+        .catch((err) => console.log(err));
+});
 
 // @route GET api/yahoo-finance/search
 // @desc Get a ticker symbol data
 // @access Private
-router.get(
-  "/search",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+router.get('/search', passport.authenticate('jwt', { session: false }), (req, res) => {
     yahooFinance
-      .search(req.query.symbol)
-      .then((tickerData) => res.json(tickerData))
-      .catch((err) => console.log(err));
-  }
-);
+        .search(req.query.symbol, {}, { validateResult: false })
+        .then((tickerData) => res.json(tickerData))
+        .catch((err) => console.log(err));
+});
 
 module.exports = router;
