210d505f47cbe713bbb8b41b131bb9a06690bbd5
Sam <sami.t.arar@hotmail.com>
2022-06-05 14:34:48 -0400

    Collapse Navbar on Link Selection

--- client/src/components/layout/NavigationBar.js
+++ client/src/components/layout/NavigationBar.js
@@ -1,68 +1,74 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
 
-class NavigationBar extends Component {
+export const NavigationBar = (props) => {
+    const [expanded, setExpanded] = useState(false);
+
     // Logout
-    onLogoutClick = (e) => {
+    const onLogoutClick = (e) => {
         e.preventDefault();
-        this.props.logoutUser();
+        props.logoutUser();
     };
 
-    render() {
-        const { user } = this.props.auth;
+    const { user } = props.auth;
+    const { isAuthenticated } = props.auth;
 
-        return (
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Container style={{ padding: '0px' }}>
-                    <Navbar.Brand as={Link} to="/">
-                        Money Trail
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link as={Link} to="/investments">
-                                Investments
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/transactions">
-                                Transactions
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/statistics">
-                                Statistics
-                            </Nav.Link>
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded}>
+            <Container style={{ padding: '0px' }}>
+                <Navbar.Brand as={Link} to="/">
+                    Money Trail
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} onClick={() => setExpanded(false)} to="/investments">
+                            Investments
+                        </Nav.Link>
+                        <Nav.Link as={Link} onClick={() => setExpanded(false)} to="/transactions">
+                            Transactions
+                        </Nav.Link>
+                        <Nav.Link as={Link} onClick={() => setExpanded(false)} to="/statistics">
+                            Statistics
+                        </Nav.Link>
 
-                            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="/settings-categories">
                                     Manage Categories
                                 </NavDropdown.Item>
                             </NavDropdown> */}
-                        </Nav>
+                    </Nav>
 
-                        <Nav>
-                            {this.props.auth.isAuthenticated ? (
-                                <NavDropdown title={user.name.split(' ')[0]} id="basic-nav-dropdown">
-                                    <NavDropdown.Item as={Link} to="/manage">
-                                        Manage
-                                    </NavDropdown.Item>
+                    <Nav>
+                        {isAuthenticated ? (
+                            <NavDropdown title={user.name.split(' ')[0]} id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} onClick={() => setExpanded(false)} to="/manage">
+                                    Manage
+                                </NavDropdown.Item>
 
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="" onClick={this.onLogoutClick}>
-                                        Logout
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            ) : (
-                                <></>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        );
-    }
-}
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item
+                                    href=""
+                                    onClick={(e) => {
+                                        setExpanded(false);
+                                        onLogoutClick(e);
+                                    }}>
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <></>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
 
 const mapStateToProps = (state) => ({
     auth: state.auth
