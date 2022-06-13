a6fa5fdbb7e41d5e9bd4b20851a295f7f4ebb790
Sam <sami.t.arar@hotmail.com>
2022-06-13 17:11:01 -0400

    Fixed Month Picker Calendar Display causing White Space below footer

--- src/components/statistics/MonthPicker.js
+++ src/components/statistics/MonthPicker.js
@@ -1,5 +1,6 @@
+import { callback } from 'chart.js/helpers';
 import React, { useEffect, useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import Picker from 'react-month-picker';
 import 'react-month-picker/css/month-picker.css';
 
@@ -32,19 +33,24 @@ const MonthPicker = ({ range, setChartDate, defaultDate }) => {
     };
 
     return (
-        <Container>
-            <Button variant="outline-dark" size="sm" onClick={showMonthPicker}>
-                {getMonthValue()}
-            </Button>
-            <Picker
-                show={isVisible}
-                lang={monthNamesShort}
-                years={range}
-                value={monthYear}
-                onChange={handleOnChange}
-                onDismiss={handleOnDismiss}
-                theme="dark"
-            />
+        <Container className="p-0">
+            <Row className="m-0 position-absolute" style={{ top: '-292.73px', left: 'calc(50% - 166.39px)', justifyContent: 'center' }}>
+                <Picker
+                    show={isVisible}
+                    lang={monthNamesShort}
+                    years={range}
+                    value={monthYear}
+                    onChange={handleOnChange}
+                    onDismiss={handleOnDismiss}
+                    theme="dark"
+                />
+            </Row>
+
+            <Row className="m-0 mt-1 justify-content-center w-100 h-100">
+                <Button variant="outline-dark" size="sm" onClick={showMonthPicker}>
+                    {getMonthValue()}
+                </Button>
+            </Row>
         </Container>
     );
 };
