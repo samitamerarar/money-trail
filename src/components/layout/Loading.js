4dbf656e60fd39712147d9c49ccfd54c7402d778
Sam <sami.t.arar@hotmail.com>
2022-06-14 20:54:39 -0400

    Added loading for Images and fixed Categories buttons filters on Transactions Table

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Row, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import ScaleLoader from 'react-spinners/ScaleLoader';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 export const Loading = (props) => {
+    const [timeoutLoading, setTimeoutLoading] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setTimeoutLoading(true), 3000);
+    }, []);
+
     return (
-        <Container className="mt-5">
-            <Row className="justify-content-center m-3">Loading {props.loadingwhat}...</Row>
-            <Row className="justify-content-center">
-                <ScaleLoader color={'#007bff'} speedMultiplier={1} />
-            </Row>
-        </Container>
+        <>
+            {props.small ? (
+                <Row className="p-0 m-0">
+                    <ClipLoader color={'#007bff'} speedMultiplier={1} size={20} />
+                    <div className="ml-2">Loading {props.loadingwhat}...</div>
+                </Row>
+            ) : (
+                <Container className="mt-5 mb-5">
+                    <Row className="justify-content-center m-3">Loading {props.loadingwhat}...</Row>
+                    <Row className="justify-content-center">
+                        <ScaleLoader color={'#007bff'} speedMultiplier={1} />
+                    </Row>
+                </Container>
+            )}
+            {timeoutLoading ? <>It's broken?</> : <></>}
+        </>
     );
 };
 
--- src/components/transactions/content/CategoryImage.js
+++ src/components/transactions/content/CategoryImage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Container, Col, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -13,17 +13,34 @@ import fun from '../assets/fun.png';
 import medical from '../assets/medical.png';
 import other from '../assets/other.png';
 import personalcare from '../assets/personalcare.png';
+import Loading from '../../layout/Loading';
 
 const images = [all, amenities, automobile, clothing, electronics, food, fun, medical, other, personalcare];
 
 export const CategoryImage = ({ image }) => {
     const [categoryImage, setCategoryImage] = useState(all);
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     useEffect(() => {
         image && setCategoryImage(images.filter((i) => i.indexOf(image) !== -1)[0]);
     }, [image]);
 
-    return <Image style={{ opacity: '0.75' }} src={categoryImage ? categoryImage : other} height="192px" className="p-4 shadow-strong" />;
+    return (
+        <Container className="mb-3">
+            <Row>
+                <Col className="text-center">
+                    {imageLoaded ? null : <Loading loadingwhat="image" />}
+                    <Image
+                        style={imageLoaded ? { opacity: '0.75' } : { display: 'none' }}
+                        src={categoryImage ? categoryImage : other}
+                        height="192px"
+                        className="p-3 shadow-strong"
+                        onLoad={() => setImageLoaded(true)}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 CategoryImage.propTypes = {
