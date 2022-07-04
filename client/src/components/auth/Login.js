c939cf5b6d1e6fd6f412550e1179de035780062e
Sam <sami.t.arar@hotmail.com>
2022-07-04 17:05:13 -0400

    Modified Routes Logic to accommodate react-router-dom 6

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Container, Button, Row, Col, Card } from 'react-bootstrap';
 
 import PropTypes from 'prop-types';
@@ -11,151 +11,136 @@ import Loading from '../layout/Loading';
 
 const isEmpty = require('is-empty');
 
-class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {}
-        };
-    }
+export const Login = (props) => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
 
-    componentDidMount() {
+    useEffect(() => {
         // If logged in and user navigates to Login page, should redirect them to dashboard
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/transactions');
+        if (props.auth.isAuthenticated) {
+            navigate('/transactions');
         }
-    }
+    }, []);
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/transactions'); // push user to dashboard when they login
+    useEffect(() => {
+        if (props.auth.isAuthenticated) {
+            navigate('/transactions'); // push user to dashboard when they login
         }
-        if (nextProps.errors) {
-            this.setState({
-                errors: nextProps.errors
-            });
+        if (props.errors) {
+            setErrors(props.errors);
         }
-    }
+    }, [props.errors, props.auth.isAuthenticated]);
 
-    onChange = (e) => {
-        this.setState({ [e.target.id]: e.target.value });
-    };
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
         const userData = {
-            email: this.state.email,
-            password: this.state.password
+            email: email,
+            password: password
         };
 
-        // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
-        this.props.loginUser(userData);
+        props.loginUser(userData);
     };
 
-    render() {
-        const { errors } = this.state;
-        console.log(this.props.errors);
-        return (
-            <Container>
-                <Link to="/" className="btn-flat waves-effect">
-                    Back to home
-                </Link>
-                <Row className="p-1">
-                    <Col>
-                        <h4>Login below</h4>
-                    </Col>
+    return (
+        <Container>
+            <Link to="/" className="btn-flat waves-effect">
+                Back to home
+            </Link>
+            <Row className="p-1">
+                <Col>
+                    <h4>Login below</h4>
+                </Col>
+            </Row>
+
+            {!isEmpty(props.errors) && (
+                <Row>
+                    <Col className="text-center text-danger">Errors were returned by the server.</Col>
                 </Row>
+            )}
 
-                {!isEmpty(this.props.errors) && (
-                    <Row>
-                        <Col className="text-center text-danger">Errors were returned by the server.</Col>
+            {props.auth.loading ? (
+                <Loading loadingwhat="server" />
+            ) : (
+                <>
+                    <UnDraw image={'undraw_access_account_re_8spm'} size="20vh" subtitle="Secure login (with passport and JWTs)." />
+
+                    <Row className="justify-content-center mb-5">
+                        <Card style={{ width: '20rem' }}>
+                            <Card.Body>
+                                <form noValidate onSubmit={onSubmit}>
+                                    <Row className="pb-1">
+                                        <Col>Email</Col>
+                                    </Row>
+                                    <Row className="pb-2">
+                                        <Col className="text-center">
+                                            <input
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                value={email}
+                                                error={errors.email}
+                                                id="email"
+                                                type="email"
+                                                className={classnames('w-100', {
+                                                    invalid: errors.email || errors.emailnotfound
+                                                })}
+                                            />
+                                            <Col className="text-left p-0">
+                                                <small className="text-danger">
+                                                    {errors.email}
+                                                    {errors.emailnotfound}
+                                                </small>
+                                            </Col>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="pb-1">
+                                        <Col>Password</Col>
+                                    </Row>
+                                    <Row className="pb-3">
+                                        <Col className="text-center">
+                                            <input
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                value={password}
+                                                error={errors.password}
+                                                id="password"
+                                                type="password"
+                                                className={classnames('w-100', {
+                                                    invalid: errors.password || errors.passwordincorrect
+                                                })}
+                                            />
+                                            <Col className="text-left p-0">
+                                                <small className="text-danger">
+                                                    {errors.password}
+                                                    {errors.passwordincorrect}
+                                                </small>
+                                            </Col>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="pb-3">
+                                        <Col className="text-center">
+                                            <Button className="w-100" type="submit" variant="primary">
+                                                Login
+                                            </Button>
+                                        </Col>
+                                    </Row>
+
+                                    <Row>
+                                        <Col className="text-center">
+                                            Don't have an account? <Link to="/register">Register</Link>
+                                        </Col>
+                                    </Row>
+                                </form>
+                            </Card.Body>
+                        </Card>
                     </Row>
-                )}
-
-                {this.props.auth.loading ? (
-                    <Loading loadingwhat="server" />
-                ) : (
-                    <>
-                        <UnDraw image={'undraw_access_account_re_8spm'} size="20vh" subtitle="Secure login (with passport and JWTs)." />
-
-                        <Row className="justify-content-center mb-5">
-                            <Card style={{ width: '20rem' }}>
-                                <Card.Body>
-                                    <form noValidate onSubmit={this.onSubmit}>
-                                        <Row className="pb-1">
-                                            <Col>Email</Col>
-                                        </Row>
-                                        <Row className="pb-2">
-                                            <Col className="text-center">
-                                                <input
-                                                    onChange={this.onChange}
-                                                    value={this.state.email}
-                                                    error={errors.email}
-                                                    id="email"
-                                                    type="email"
-                                                    className={classnames('w-100', {
-                                                        invalid: errors.email || errors.emailnotfound
-                                                    })}
-                                                />
-                                                <Col className="text-left p-0">
-                                                    <small className="text-danger">
-                                                        {errors.email}
-                                                        {errors.emailnotfound}
-                                                    </small>
-                                                </Col>
-                                            </Col>
-                                        </Row>
-
-                                        <Row className="pb-1">
-                                            <Col>Password</Col>
-                                        </Row>
-                                        <Row className="pb-3">
-                                            <Col className="text-center">
-                                                <input
-                                                    onChange={this.onChange}
-                                                    value={this.state.password}
-                                                    error={errors.password}
-                                                    id="password"
-                                                    type="password"
-                                                    className={classnames('w-100', {
-                                                        invalid: errors.password || errors.passwordincorrect
-                                                    })}
-                                                />
-                                                <Col className="text-left p-0">
-                                                    <small className="text-danger">
-                                                        {errors.password}
-                                                        {errors.passwordincorrect}
-                                                    </small>
-                                                </Col>
-                                            </Col>
-                                        </Row>
-
-                                        <Row className="pb-3">
-                                            <Col className="text-center">
-                                                <Button className="w-100" type="submit" variant="primary">
-                                                    Login
-                                                </Button>
-                                            </Col>
-                                        </Row>
-
-                                        <Row>
-                                            <Col className="text-center">
-                                                Don't have an account? <Link to="/register">Register</Link>
-                                            </Col>
-                                        </Row>
-                                    </form>
-                                </Card.Body>
-                            </Card>
-                        </Row>
-                    </>
-                )}
-            </Container>
-        );
-    }
-}
+                </>
+            )}
+        </Container>
+    );
+};
 
 Login.propTypes = {
     loginUser: PropTypes.func.isRequired,
